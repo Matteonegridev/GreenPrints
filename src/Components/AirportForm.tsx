44034aca66ee25e3275bdcffedb3ff6b8739e09c
Matteo <matteonegridev@gmail.com>
2024-11-06 19:20:53 +0100

    created filter functions

--- src/Components/AirportForm.tsx
+++ src/Components/AirportForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { type Airport } from "../Hooks/useAirportData";
 import useAirportData from "../Hooks/useAirportData";
 import useClimateData from "../Hooks/useClimateData";
@@ -8,12 +8,18 @@ import { useDebounce } from "use-debounce";
 function AirportForm() {
   const [origin, setOrigin] = useState("");
   const [destination, setDestination] = useState("");
-  const [suggestions, setSuggestions] = useState<Airport[]>([]);
+  const [suggestions, setSuggestions] = useState<{
+    origin: Airport[];
+    destination: Airport[];
+  }>({
+    origin: [],
+    destination: [],
+  });
   const [originSearch, setOriginSearch] = useState("");
   const [destinationSearch, setDestinationSearch] = useState("");
   const [passengers, setPassengers] = useState(1);
-  const [debounceOrigin] = useDebounce(originSearch, 1000);
-  const [debounceDestination] = useDebounce(destinationSearch, 1000);
+  const [debounceOrigin] = useDebounce(originSearch, 200);
+  const [debounceDestination] = useDebounce(destinationSearch, 200);
 
   const { data, isLoading, error } = useAirportData();
   const {
@@ -22,8 +28,52 @@ function AirportForm() {
     data: climatedata,
   } = useClimateData(origin, destination, passengers);
 
+  // useEffect per il debounce cosi che il dato è richiamato quando si smette di scrivere:
+  useEffect(() => {
+    if (debounceOrigin) {
+      handleFilterItems(data, debounceOrigin, "origin");
+
+      console.log(debounceOrigin);
+    } else {
+      setSuggestions((prev) => ({ ...prev, origin: [] }));
+    }
+  }, [data, debounceOrigin]);
+
+  // useEffect per il debounce cosi che il dato è richiamato quando si smette di scrivere:
+  useEffect(() => {
+    if (debounceDestination) {
+      handleFilterItems(data, debounceDestination, "destination");
+    } else {
+      setSuggestions((prev) => ({ ...prev, destination: [] }));
+    }
+  }, [data, debounceDestination]);
+
   if (isLoading && load) return <p>Loading...</p>;
   if (error && err) return <p>Something has occurred...</p>;
+
+  // funzione filter passata allo useEffect in modo da poter utilizzare il debounce:
+  const handleFilterItems = (
+    data: Airport,
+    query: string,
+    type: "origin" | "destination"
+  ) => {
+    const trimmedQuery = query.trim();
+    if (trimmedQuery === "") {
+      setSuggestions((prev) => ({ ...prev, [type]: [] }));
+      return;
+    }
+
+    const filteredElements = Array.isArray(data)
+      ? data.filter(
+          (values: Airport) =>
+            values.name.toLowerCase().includes(trimmedQuery.toLowerCase()) ||
+            values.country.toLowerCase().includes(trimmedQuery.toLowerCase()) ||
+            values.code.toLowerCase().includes(trimmedQuery.toLowerCase())
+        )
+      : [];
+
+    setSuggestions((prev) => ({ ...prev, [type]: filteredElements }));
+  };
 
   const handleClickFromListOrigin = (e: React.MouseEvent<HTMLUListElement>) => {
     const li = (e.target as HTMLElement).closest("li");
@@ -32,7 +82,7 @@ function AirportForm() {
       console.log("clicked: ", searchItem);
       setOriginSearch(searchItem || "");
       setOrigin(searchItem || "");
-      setSuggestions([]);
+      setSuggestions((prev) => ({ ...prev, origin: [] }));
     }
   };
 
@@ -45,42 +95,18 @@ function AirportForm() {
       console.log("clicked: ", searchItem);
       setDestinationSearch(searchItem || "");
       setDestination(searchItem || "");
-      setSuggestions([]);
+      setSuggestions((prev) => ({ ...prev, destination: [] }));
     }
   };
 
   const handleChangeOrigin = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setOriginSearch(value);
-    handleFilterItems(data, value, "origin");
   };
 
   const handleChangeDestination = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setDestinationSearch(value);
-    handleFilterItems(data, value, "destination");
-  };
-
-  const handleFilterItems = (
-    data: Airport,
-    query: string,
-    type: "origin" | "destination"
-  ) => {
-    const trimmedQuery = query.trim();
-    if (trimmedQuery === "") {
-      setSuggestions([]);
-      return;
-    }
-
-    const filteredElements = Array.isArray(data)
-      ? data.filter(
-          (values: Airport) =>
-            values.name.toLowerCase().includes(trimmedQuery.toLowerCase()) ||
-            values.country.toLowerCase().includes(trimmedQuery.toLowerCase()) ||
-            values.code.toLowerCase().includes(trimmedQuery.toLowerCase())
-        )
-      : [];
-    setSuggestions(filteredElements);
   };
 
   return (
@@ -91,7 +117,7 @@ function AirportForm() {
           text="From:"
           onChange={handleChangeOrigin}
           value={originSearch}
-          airportData={suggestions}
+          airportData={suggestions.origin}
           onClick={handleClickFromListOrigin}
         />
         <AirportInput
@@ -99,7 +125,7 @@ function AirportForm() {
           text="To:"
           onChange={handleChangeDestination}
           value={destinationSearch}
-          airportData={suggestions}
+          airportData={suggestions.destination}
           onClick={handleClickFromListDestination}
         />
       </form>
