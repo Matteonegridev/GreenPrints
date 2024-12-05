726f3669bd0714315a56123129a7101d5abaf024
Matteo <matteonegridev@gmail.com>
2024-12-05 11:25:57 +0100

    random commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    // (https://www.youtube.com/watch?v=N4yUiQiTvwU)
     proxy: {
       "/api": {
         target: "http://localhost:5000",
