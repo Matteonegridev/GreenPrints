79d1a24dfb2a5406cb85b3642fc406708ef4f33f
Matteo <matteonegridev@gmail.com>
2024-12-06 14:51:53 +0100

    done switch lang button

--- src/Components/Routes/RootLayout.tsx
+++ src/Components/Routes/RootLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
+import LangSwitch from "../../Utils/LangSwitch";
 
 function RootLayout() {
   return (
@@ -8,6 +9,7 @@ function RootLayout() {
       <Navbar />
       <Outlet />
       <Footer />
+      <LangSwitch />
     </>
   );
 }
---
+++ src/Utils/LangSwitch.tsx
@@ -0,0 +1,27 @@
+import i18next from "i18next";
+import { useTranslation } from "react-i18next";
+import { FlagIcon } from "react-flag-kit";
+
+function LangSwitch() {
+  const { i18n } = useTranslation();
+
+  const changeLanguage = () => {
+    const changeLang = i18n.language === "it" ? "en" : "it";
+    i18next.changeLanguage(changeLang);
+  };
+
+  const changeFlag = () => {
+    return i18n.language === "it" ? "GB" : "IT";
+  };
+
+  return (
+    <button
+      className="fixed bottom-10 right-3 flex h-16 w-16 items-center justify-center rounded-full bg-clearDark px-4 py-6 shadow-lg dark:bg-gray-50"
+      onClick={changeLanguage}
+    >
+      <FlagIcon code={changeFlag()} size={32} />
+    </button>
+  );
+}
+
+export default LangSwitch;
